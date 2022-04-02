a29c675d37b4b6752b08ae592fa6e5f4be86a539
Hynesman <83984942+Hynesman@users.noreply.github.com>
2022-04-03 00:05:16 +0200

    library install script and connection.py change
    added a library update script.
    and minor change to the internet connection check, to make it faster.

--- 3G_sakis3g_mod/copy_services.sh
+++ 3G_sakis3g_mod/copy_services.sh
@@ -1,13 +1,22 @@
 #
 #!/bin/bash
 
+sudo chmod +x is_there_internet
+sudo cp is_there_internet /usr/bin/is_there_internet
+sudo chmod +x connection
 sudo cp connection /usr/bin/connection
 echo "copied connection to /usr/bin/connection"
+sudo chmod +x NET_CONNECT
 sudo cp NET_CONNECT /usr/bin/NET_CONNECT
 echo "copied NET_CONNECT to /etc/bin/NET_CONNECT"
+sudo chmod +x connection.service
 sudo cp connection.service /etc/systemd/system/connection.service
 echo "copied connection.service to /etc/systemd/system/connection.service"
 
 echo "finished"
 echo "enable service with systemctl for it to take effect :)"
+echo "type ( sudo systemctl enable connection.service ) "
+echo "then ( sudo systemctl start connection.service ) "
+
+echo "also ( sudo reboot )"
 
---
+++ library install scripts/install_req_lib.sh
@@ -0,0 +1,18 @@
+#
+#!/bin/bash
+
+sudo apt update
+export LANG=en_DK.UTF-8
+sudo apt-get install locales
+sudo update-locale LANG=en_DK.UTF-8
+sudo apt update && sudo apt upgrade -y
+sudo apt install vim mc
+sudo apt install software-properties-common -y
+sudo apt install git -y
+sudo apt install python3-pip
+
+sudo apt-get install usbutils
+sudo apt-get install usb-modeswitch
+sudo apt-get install ppp          
+
+echo "reboot the system now please :)"
