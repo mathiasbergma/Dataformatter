2b6721816bbd3ba1145a3602d442912ba26b39d5
MathiasBergma <mathiasbergma@gmail.com>
2022-04-19 22:19:29 +0200

    Hej anders

---
+++ Server_docker/db/01-init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+export PGPASSWORD = $POSTGRES_PASSWORD;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE ROLE web_anon NOLOGIN;
+    CREATE ROLE authenticator NOINHERIT LOGIN PASSWORD '$AUTHENTICATOR_PASS';
+    GRANT web_anon TO authenticator;
+    CREATE SCHEMA IF NOT EXISTS api;
+    CREATE TABLE api.gokarts (
+        id SERIAL PRIMARY KEY,
+        gokart VARCHAR(80) NOT NULL,
+        time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        signal VARCHAR(80),
+        value REAL,
+        unit VARCHAR(80),
+        power_state VARCHAR(80)
+    );
+    GRANT usage ON SCHEMA api TO web_anon;
+    GRANT SELECT ON api.gokarts TO web_anon;
+EOSQL
+
+
