b2fe342369a94dc1a02b293d6e9466915c74f2ef
MathiasBergma <mathiasbergma@gmail.com>
2022-04-29 13:26:57 +0200

    Corrected

--- install_PB.sh
+++ install_PB.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-apt update && apt install -y \
-    vim \
-    curl \
-    make \
-    gcc \
-    build-essential \
-    libssl-dev \
-    git \
+apt update && apt install -y vim curl make gcc build-essential libssl-dev git 
 git clone https://github.com/eclipse/paho.mqtt.c.git
 cd paho.mqtt.c
 RUN make
