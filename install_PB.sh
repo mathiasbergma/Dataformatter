9db01ee58f6c33a1c812626465b0c132646dfd6e
Demo User <debian@beaglebone.localdomain>
2022-05-04 12:04:55 +0000

    Changed such that downloads happen in home directory

--- install_PB.sh
+++ install_PB.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 apt update && apt install -y vim curl make gcc build-essential libssl-dev git software-properties-common libnss3-dev zlib1g-dev libgdbm-dev libncurses5-dev libssl-dev libffi-dev libreadline-dev libsqlite3-dev libbz2-dev
+cd
 mkdir Downloads
 cd Downloads
 git clone https://github.com/eclipse/paho.mqtt.c.git
