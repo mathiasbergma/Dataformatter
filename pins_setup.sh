6cffac2d89039df178a91d5951f2f4c1acdfb0b8
MathiasBergma <mathiasbergma@gmail.com>
2022-05-04 15:32:24 +0200

    Changed CAN speed

--- pins_setup.sh
+++ pins_setup.sh
@@ -5,5 +5,5 @@ config-pin p1_28 can
 config-pin p2_25 can
 config-pin p2_27 can
 
-ip link set can0 up type can bitrate 125000
-ip link set can1 up type can bitrate 125000
+ip link set can0 up type can bitrate 50000
+ip link set can1 up type can bitrate 50000
