813b01f24c366a72c1a999ecf33683d5f589d2f1
MathiasBergma <mathiasbergma@gmail.com>
2022-05-04 09:17:39 +0200

    nfow

--- boot.sh
+++ boot.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+#todo Connect to internet through 4G modem
 ./pins_setup.sh
-sh -c './home/debian/Gokart_CAN_API/Onboard_CAN_MQTT_Client/CANsniff.py & ./home/debian/Gokart_CAN_API/Power_off/Pocket_shutdown'
+sh -c './home/debian/Gokart_CAN_API/Onboard_CAN_MQTT_Client/CANsniff.py & \
+	./home/debian/Gokart_CAN_API/Power_off/Pocket_shutdown'
