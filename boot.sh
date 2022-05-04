fa6881df8018067f30bb68e1f20c6a8ab3d986d3
Demo User <debian@beaglebone.localdomain>
2022-05-04 08:56:48 +0000

    Updated paths in script

--- boot.sh
+++ boot.sh
@@ -2,5 +2,5 @@
 
 #todo Connect to internet through 4G modem
 ./pins_setup.sh
-sh -c './home/debian/Gokart_CAN_API/Onboard_CAN_MQTT_Client/CANsniff.py & \
-	./home/debian/Gokart_CAN_API/Power_off/Pocket_shutdown'
+sh -c '/home/debian/Gokart_CAN_API/Onboard_CAN_MQTT_Client/CANsniff.py & \
+	/home/debian/Gokart_CAN_API/Power_off/Pocket_shutdown'
