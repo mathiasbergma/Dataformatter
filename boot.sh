f3c7cbd1319662548b5b562141e64e10d17a0fa3
Hynesman <83984942+Hynesman@users.noreply.github.com>
2022-05-04 14:31:37 +0200

    Update boot.sh
    added GPS_usb parcer to boot

--- boot.sh
+++ boot.sh
@@ -5,4 +5,5 @@
 sleep 30
 ./pins_setup.sh
 sh -c '/home/debian/Gokart_CAN_API/Onboard_CAN_MQTT_Client/CANsniff.py & \
-	/home/debian/Gokart_CAN_API/Power_off/Pocket_shutdown'
+	/home/debian/Gokart_CAN_API/Power_off/Pocket_shutdown \
+	/home/debian/Gokart_CAN_API/GPS_USB_NMEA_PARCER/Debug/GPS_USB_NMEA_PARCER'
