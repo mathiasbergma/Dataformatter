dbc7f2edcc408467c811ea251dc8e6cdbe9ba06a
MathiasBergma <mathiasbergma@gmail.com>
2022-05-04 11:37:21 +0200

    Corrected spelling error

--- pins_setup.sh
+++ pins_setup.sh
@@ -3,7 +3,7 @@
 config-pin p1_26 can
 config-pin p1_28 can
 config-pin p2_25 can
-config-pin p2.27 can
+config-pin p2_27 can
 
 ip link set can0 up type can bitrate 125000
 ip link set can0 up type can bitrate 125000
