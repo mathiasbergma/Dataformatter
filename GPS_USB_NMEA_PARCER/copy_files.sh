f6aea71a349a85894eb0a5a5491a9b1554655e8e
Hynesman <83984942+Hynesman@users.noreply.github.com>
2022-05-15 23:16:04 +0200

    Update copy_files.sh

--- GPS_USB_NMEA_PARCER/copy_files.sh
+++ GPS_USB_NMEA_PARCER/copy_files.sh
@@ -2,7 +2,4 @@
 #!/bin/bash
 
 sudo cp 99-usb-serial.rules /etc/udev/rules.d/99-usb-serial.rules
-echo "copied connection to /usr/bin/connection"
-echo "finished"
-echo "also ( sudo reboot )"
-
+echo "finished, remember to reboot"
