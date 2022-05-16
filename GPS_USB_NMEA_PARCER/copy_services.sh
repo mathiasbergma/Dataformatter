d63076b7fb675795257412614b9f0babfdfa0707
root <root@beaglebone.localdomain>
2022-05-16 19:59:52 +0000

    setup GPS.service

--- GPS_USB_NMEA_PARCER/copy_services.sh
+++ GPS_USB_NMEA_PARCER/copy_services.sh
@@ -1,9 +1,9 @@
 #
 #!/bin/bash
 
-sudo chmod +x /Debug/GPS_USB_NMEA_PARCER
+sudo chmod +x ./Debug/GPS_USB_NMEA_PARCER
 sudo cp ./Debug/GPS_USB_NMEA_PARCER /usr/bin/GPS_USB_NMEA_PARCER
-echo "copied ./Debug/GPS_USB_NMEA_PARCER to /usr/bin/GPS_USB_NMEA_PARCER"
+echo "copied Debug/GPS_USB_NMEA_PARCER to /usr/bin/GPS_USB_NMEA_PARCER"
 sudo chmod +x GPS.service
 sudo cp GPS.service /etc/systemd/system/GPS.service
 echo "copied GPS.service to /etc/systemd/system/GPS.service"
